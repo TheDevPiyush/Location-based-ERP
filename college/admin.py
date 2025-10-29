051124fe4138ba6a49c4d7482f14feaeb1085b0e
Piyush Choudhary <contactpiyushhere@gmail.com>
2025-10-29 16:11:05 +0530

    renamed users folder to college

--- college/admin.py
+++ college/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, University, Course, Batch, Subject
+admin.site.register(User)
+admin.site.register(University)
+admin.site.register(Course)
+admin.site.register(Batch)
+admin.site.register(Subject)
