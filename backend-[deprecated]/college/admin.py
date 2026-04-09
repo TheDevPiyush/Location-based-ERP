e518fe9c58608326d162e08b3b4f56c53fe85958
Piyush Choudhary <contactpiyushhere@gmail.com>
2026-04-09 22:33:04 +0530

    renamed folder structure

--- backend-[deprecated]/college/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import User, University, Course, Batch, Subject
-admin.site.register(User)
-admin.site.register(University)
-admin.site.register(Course)
-admin.site.register(Batch)
-admin.site.register(Subject)
